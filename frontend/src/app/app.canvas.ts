1c36ccaea13ed493ceecbb8ec7c830781901a105
Marc Berchtold <marc.berchtold@hotmail.de>
2017-05-05 11:50:42 +0200

    Bounding rect and changed websocket protocol

--- frontend/src/app/app.canvas.ts
+++ frontend/src/app/app.canvas.ts
@@ -5,20 +5,30 @@ import * as _ from "lodash";
 
 @Component({
   selector: 'app-canvas',
-  template: '<canvas #canvas></canvas>',
+  template: '<canvas #canvas id="canvas"></canvas><canvas #canvasOverlay id="canvasOverlay"></canvas>',
   styleUrls: ['app.canvas.scss']
 })
 export class CanvasComponent implements AfterViewInit {
   //Reference to the canvas from the template
   @ViewChild('canvas') public canvas: ElementRef;
+  @ViewChild('canvasOverlay') public canvasOverlay: ElementRef;
 
   //Height & Width for the canvas
   @Input() public width = 400;
   @Input() public height = 400;
+  @Input() public strokeWidth = 10;
 
   private cx: CanvasRenderingContext2D;
+  private cxOverlay: CanvasRenderingContext2D;
   private canvasEl: HTMLCanvasElement;
+  private canvasOverlayEl: HTMLCanvasElement;
   private socketIO;
+  private borderPos = {
+    minX: -1,
+    minY: -1,
+    maxX: -1,
+    maxY: -1,
+  };
 
   constructor(socketIO: SocketIO) {
     this.socketIO = socketIO;
@@ -27,16 +37,21 @@ export class CanvasComponent implements AfterViewInit {
   ngAfterViewInit() {
     //get context
     this.canvasEl = this.canvas.nativeElement;
+    this.canvasOverlayEl = this.canvasOverlay.nativeElement;
     this.cx = this.canvasEl.getContext('2d');
+    this.cxOverlay = this.canvasOverlayEl.getContext('2d');
 
     //set width & height
     this.canvasEl.width = this.width;
     this.canvasEl.height = this.height;
+    this.canvasOverlayEl.width = this.width;
+    this.canvasOverlayEl.height = this.height;
 
     //set draw settings
-    this.cx.lineWidth = 10;
+    this.cx.lineWidth = this.strokeWidth;
     this.cx.lineCap = 'round';
     this.cx.strokeStyle = '#000000';
+    this.cxOverlay.strokeStyle = '#000000';
 
     this.cx.fillStyle = "#ffffff";
     this.cx.fillRect(0,0,this.width, this.height);
@@ -101,11 +116,33 @@ export class CanvasComponent implements AfterViewInit {
       return;
 
     this.cx.beginPath();
+    this.cxOverlay.beginPath();
 
     if(prevPos) {
       this.cx.moveTo(prevPos.x, prevPos.y); //from
       this.cx.lineTo(currentPos.x, currentPos.y); //to
       this.cx.stroke(); //apply the stroke settings set in ngAfterViewInit
+
+      if(currentPos.x < this.borderPos.minX + this.strokeWidth || this.borderPos.minX == -1) {
+        this.borderPos.minX = currentPos.x - this.strokeWidth;
+        this.borderPos.minX = this.borderPos.minX < 0 ? 0 : this.borderPos.minX;
+      }
+      if(currentPos.y < this.borderPos.minY + this.strokeWidth || this.borderPos.minY == -1) {
+        this.borderPos.minY = currentPos.y - this.strokeWidth;
+        this.borderPos.minY = this.borderPos.minY < 0 ? 0 : this.borderPos.minY;
+      }
+      if(currentPos.x > this.borderPos.maxX - this.strokeWidth || this.borderPos.maxX == -1) {
+        this.borderPos.maxX = currentPos.x + this.strokeWidth;
+        this.borderPos.maxX = this.borderPos.maxX > this.width ? this.width : this.borderPos.maxX;
+      }
+      if(currentPos.y > this.borderPos.maxY - this.strokeWidth || this.borderPos.maxY == -1) {
+        this.borderPos.maxY = currentPos.y + this.strokeWidth;
+        this.borderPos.maxY = this.borderPos.maxY > this.height ? this.height : this.borderPos.maxY;
+      }
+
+      this.cxOverlay.clearRect(0,0,this.width, this.height);
+      this.cxOverlay.rect(this.borderPos.minX, this.borderPos.minY, this.borderPos.maxX-this.borderPos.minX, this.borderPos.maxY-this.borderPos.minY);
+      this.cxOverlay.stroke();
     }
   }
 
@@ -136,7 +173,12 @@ export class CanvasComponent implements AfterViewInit {
   }
 
   private sendBase64PNG = _.throttle(() => {
-    this.socketIO.sendMessage('read', this.canvas.nativeElement.toDataURL());
+    this.socketIO.sendMessage('read', {
+      image: this.canvas.nativeElement.toDataURL(),
+      x: ((this.borderPos.maxX - this.borderPos.minX) / 2) + this.borderPos.minX,
+      y: ((this.borderPos.maxY - this.borderPos.minY) / 2) + this.borderPos.minY,
+      d: Math.max(this.borderPos.maxX - this.borderPos.minX, this.borderPos.maxY - this.borderPos.minY)
+    });
   }, 250);
 
   public clearRect() {
@@ -145,5 +187,10 @@ export class CanvasComponent implements AfterViewInit {
 
     this.cx.fillStyle = "#ffffff";
     this.cx.fillRect(0,0,this.width, this.height);
+    this.cxOverlay.clearRect(0,0,this.width, this.height);
+    this.borderPos.minX = -1;
+    this.borderPos.minY = -1;
+    this.borderPos.maxX = -1;
+    this.borderPos.maxY = -1;
   }
 }
